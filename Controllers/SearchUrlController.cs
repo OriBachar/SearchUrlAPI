5e4b7d025c6f8e92cd1ac9bc03036e74b21ee917
Ori Bachar <73943348+OriBachar@users.noreply.github.com>
2022-01-30 13:26:36 +0200

    Add files via upload

---
+++ Controllers/SearchUrlController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Net;
+using HtmlAgilityPack;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Storage;
+using SearchUrlAPI.SearchUrl;
+
+namespace SearchUrlController.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchUrlController : Controller
+    {
+        [HttpGet]
+        public string Get(string url, int number)
+        {
+
+            List<string> list = new List<string>();
+
+            List<SearchUrlAPI.SearchUrl.SearchUrl> data = new List<SearchUrlAPI.SearchUrl.SearchUrl>();
+
+            list = GatAllUrls(url);
+
+            if (list is null)
+                return "The remote server returned an error";
+
+            list.RemoveAll(match => !match.StartsWith("https://"));
+
+            int minindex = number < list.Count() ? number : list.Count();
+
+            for (int i = 0; i < minindex; i++)
+            {
+                Uri uri = new Uri(list[i]);
+
+                string host = uri.Host;
+
+               if (host.Contains("https://"))
+                    host = host.Remove(host.IndexOf("https://"), "https://".Length);
+
+                if (host.Contains("www."))
+                    host = host.Remove(host.IndexOf("www."), "www.".Length);
+
+                if (host.Contains(".co.il"))
+                    host = host.Remove(host.IndexOf(".co.il"), ".co.il".Length);
+
+                if (host.Contains(".com"))
+                    host = host.Remove(host.IndexOf(".com"), ".com".Length);
+
+
+                string title = host;
+
+                data.Add(new SearchUrlAPI.SearchUrl.SearchUrl(list[i], title));
+            }
+
+            string json = JsonConvert.SerializeObject(data.ToArray(), Formatting.Indented);
+
+            return json;
+        }
+
+        private List<string> GatAllUrls(string _url)
+        {
+            var client = new WebClient();
+
+            try
+            {
+                var htmlSource = client.DownloadString(_url);
+
+                var doc = new HtmlDocument();
+
+                doc.LoadHtml(htmlSource);
+
+                return doc.DocumentNode.SelectNodes("//a[@href]").Select(node => node.Attributes["href"].Value).ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
+                return null;
+            }
+
+           
+
+            
+
+        }
+    }
+}
---
+++ Models/SearchUrl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchUrlAPI.SearchUrl
+{
+    public class SearchUrl
+    {
+        public string Url { get; set; }
+
+        public string Title { get; set; }
+
+        public SearchUrl(string url, string title)
+        {
+            this.Url = url;
+
+            this.Title = title;
+        }
+
+        public SearchUrl()
+        {
+        }
+    }
+}
