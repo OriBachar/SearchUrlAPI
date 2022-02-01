70022d9fd847b05656df8cc408b8cb0d5ea16248
Ori Bachar <73943348+OriBachar@users.noreply.github.com>
2022-02-01 23:00:08 +0200

    Add files via upload

--- src/Program.cs
+++ src/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SearchUrl
+namespace SearchUrlAPI
 {
     public class Program
     {
--- src/Startup.cs
+++ src/Startup.cs
@@ -1,21 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using StackExchange.Redis;
-using SearchUrlAPI.SearchUrl;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace SearchUrl
+namespace SearchUrlAPI
 {
     public class Startup
     {
